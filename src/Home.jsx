b548933922f1aabc08dfc82d7120d45658d5e3c9
Yogadharshni <yogdharshni06@gmail.com>
2023-03-12 16:03:35 +0530

    Added img

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ export function Home() {
 
   return (
     // <h1>Welcome to Home Page⭐</h1>
-    <img id='img-div' src="http://95.140.126.60/article-image-welcome.jpg" alt="Welcome" />
+    <img id='img-div' src="https://static.vecteezy.com/system/resources/thumbnails/000/371/208/small/1465.jpg" alt="Welcome" />
 
   );
 }
